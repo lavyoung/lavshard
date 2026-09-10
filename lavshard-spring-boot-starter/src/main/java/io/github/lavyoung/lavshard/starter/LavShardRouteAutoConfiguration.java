cf5b8d8629abf31700b465c50b9736ddd41047fe
lavyoung <lavyoung1325@outlook.com>
2026-09-10 16:31:41 +0800

    chore(starter): 整理 Spring Boot Starter 包结构

---
+++ lavshard-spring-boot-starter/src/main/java/io/github/lavyoung/lavshard/starter/LavShardRouteAutoConfiguration.java
@@ -0,0 +1,11 @@
+package io.github.lavyoung.lavshard.starter;
+
+/**
+ *
+ *
+ * @author <a href="mailto:lavyoung1325@outlook.com">lavyoung</a>
+ * @version 0.1.0
+ * @data 2026/9/10
+ */
+public class LavShardRouteAutoConfiguration {
+}
